040d9628e17bc7d561c7f169eec58d33ad256dfe
Mateusz Lencki <mateusz.lencki@gmail.com>
2019-11-26 20:07:54 +0100

    Protecting API routes with auth middleware

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,14 @@ use Laravel\Lumen\Routing\Router;
 /** @var Router $router */
 $router->get('/', 'Controller@getInfo');
 
-$router->post('/points/{type}', 'Points\CreateController@createPoint');
-$router->put('/points/{type}', 'Points\CreateController@replacePoints');
-$router->get('/points/{type}', 'Points\SearchController@getAllPoints');
-$router->get('/points/{type}/search/range', 'Points\SearchController@searchPointsInRange');
-$router->get('/points/{type}/search/name', 'Points\SearchController@searchPointsByName');
+$router->group(['middleware' => 'auth'], function () use ($router): void {
+    $router->post('/points/{type}', 'Points\CreateController@createPoint');
+    $router->put('/points/{type}', 'Points\CreateController@replacePoints');
+    $router->get('/points/{type}', 'Points\SearchController@getAllPoints');
+    $router->get('/points/{type}/search/range', 'Points\SearchController@searchPointsInRange');
+    $router->get('/points/{type}/search/name', 'Points\SearchController@searchPointsByName');
 
-$router->get('/geocoding/search', 'Geocoding\SearchController@search');
-$router->get('/geocoding/reverse', 'Geocoding\SearchController@reverse');
-$router->get('/geocoding/nearby', 'Geocoding\SearchController@nearby');
+    $router->get('/geocoding/search', 'Geocoding\SearchController@search');
+    $router->get('/geocoding/reverse', 'Geocoding\SearchController@reverse');
+    $router->get('/geocoding/nearby', 'Geocoding\SearchController@nearby');
+});
