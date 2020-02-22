cddfd0dea1a942a0f200e8414b47e8ac9b180d82
Mateusz Lencki <mateusz.lencki@gmail.com>
2020-02-22 23:41:43 +0100

    Restored PointResource

---
+++ app/Http/Resources/PointResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Grocelivery\Geolocalizer\Http\Resources;
+
+use Grocelivery\Utils\Resources\JsonResource;
+
+/**
+ * Class PointResource
+ * @package Grocelivery\Geolocalizer\Http\Resources
+ */
+class PointResource extends JsonResource
+{
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->resource['_id'],
+            'name' => $this->resource['name'],
+            'location' => $this->resource['location'],
+            'payload' => $this->resource['payload'] ?? [],
+            'type' => $this->resource['type'],
+        ];
+    }
+}
